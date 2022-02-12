844e67742b3f48319c69d6cb0bd68b5c80313ce8
mennie <WooZooLSD>
2022-02-13 00:47:41 +0900

    first step

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
-void main() {
+void main() =>
   runApp(const MyApp());
-}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
